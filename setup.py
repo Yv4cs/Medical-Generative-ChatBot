1cbe5a71a7221cc3d0ee9378c2584e5b233e957a
Charles <charlesquantran@gmail.com>
2025-04-14 02:54:54 +1000

    folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Charles Quan Tran',
+    author_email= 'charlesquantran@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
