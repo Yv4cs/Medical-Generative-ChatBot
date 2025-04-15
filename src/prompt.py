6fd75c4f57b8322ae98dd40f7c4e3cfa2779223f
Charles <charlesquantran@gmail.com>
2025-04-15 19:17:10 +1000

    second commit

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+assistant_instructions = (
+    "You are a helpful assistant designed to answer questions based on provided context. "
+    "Use the retrieved information below to construct your response. "
+    "If the answer isn't present in the context, it's okay to say you don't know. "
+    "Keep your reply concise and limited to three sentences."
+    "\n\n"
+    "{context}"
+)
---
+++ store_index.py
@@ -0,0 +1,44 @@
+from src.helper import load_pdf_file, text_split, download_hugging_face_embeddings
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('API_KEY')
+os.environ["API_KEY"] = PINECONE_API_KEY
+
+
+extracted_data=load_pdf_file(data='Data/')
+text_chunks=text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+index_name = "medicalbot"
+
+
+pc.create_index(
+    name=index_name,
+    dimension=384, 
+    metric="cosine", 
+    spec=ServerlessSpec(
+        cloud="aws", 
+        region="us-east-1"
+    ) 
+) 
+
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+from langchain_pinecone import PineconeVectorStore
+
+vectorstore = PineconeVectorStore(
+    index_name="medicalbot",
+    embedding=embeddings,
+    pinecone_api_key='PINECONE_API_KEY'
+)
+
+vectorstore.add_documents(text_chunks)
